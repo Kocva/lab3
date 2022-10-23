72260e766d82392e040670edc41a3981e20fb70a
Konstantin kazaryan <kocva2003@mail.ru>
2022-10-24 00:04:54 +0800

    ADD reduction

---
+++ lab3/Fraction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Fraction
+    {
+        public int numerator;
+        public int denumerator;
+
+        public Fraction ()
+        {
+            numerator = 0;
+            denumerator = 0;
+        }
+
+        public Fraction (int numerator, int denumerator)
+        {
+            this.numerator = numerator;
+            this.denumerator = denumerator;
+        }
+
+        public int nod (Fraction fraction)
+        {
+            int n = fraction.numerator;
+            int d = fraction.denumerator;
+            n = Math.Abs(n);
+            d = Math.Abs(d);
+            while (d != 0 && n != 0)
+            {
+                if (n % d > 0)
+                {
+                    var temp = n;
+                    n = d;
+                    d = temp % d;
+                }
+                else break;
+            }
+            if (d != 0 && n != 0) return d;
+            return 1;
+        }
+
+        public static Fraction reduction (Fraction fraction)
+        {
+            int nod = fraction.nod(fraction);
+            fraction.numerator = fraction.numerator / nod;
+            fraction.denumerator = fraction.denumerator / nod;
+            return fraction;
+        }
+    }
+}
