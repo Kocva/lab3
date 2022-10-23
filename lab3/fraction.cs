95781774a748890df100fe97af02ab9d70e03f38
Konstantin kazaryan <kocva2003@mail.ru>
2022-10-23 23:46:46 +0800

    ADD constructors

---
+++ lab3/fraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Fraction
+    {
+        public int numerator;
+        public int denumerator;
+
+        public Fraction ()
+        {
+            numerator = 0;
+            denumerator = 0;
+        }
+
+        public Fraction (int numerator, int denumerator)
+        {
+            this.numerator = numerator;
+            this.denumerator = denumerator;
+        }
+
+
+    }
+}
